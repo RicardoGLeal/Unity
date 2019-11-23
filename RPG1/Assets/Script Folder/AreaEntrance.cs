04c006af70349e5935114ad277727e74d3a7592a
Ricardo <ricardoglr664@gmail.com>
2019-11-23 17:44:09 -0600

    First Version
    Two Scenes and the Player

---
+++ RPG1/Assets/Script Folder/AreaEntrance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEntrance : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string areaTransitionName;
+    void Start()
+    {
+        if (PlayerController.instance.areaTransitionName == areaTransitionName)
+            PlayerController.instance.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RPG1/Assets/Script Folder/AreaExit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AreaExit : MonoBehaviour
+{
+    public string areaToLoad;
+    public string areaTransitionName;
+    public AreaEntrance theEntrance;
+    void Start()
+    {
+        theEntrance.areaTransitionName = areaTransitionName;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(areaToLoad);
+            PlayerController.instance.areaTransitionName = areaTransitionName;
+        }
+    }
+}
