d66a04406a3c2da448d718a08ccc3bacead541f9
Ricardo <ricardoglr664@gmail.com>
2019-11-23 21:07:55 -0600

    Camera
    -Keeping Camera inside the map
    -Hiding the map edges

--- RPG1/Assets/Script Folder/CameraController.cs
+++ RPG1/Assets/Script Folder/CameraController.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform target;
+    public Tilemap theMap;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    private float halfHeight;
+    private float halfWidth;
     void Start()
     {
         target = PlayerController.instance.transform;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+        bottomLeftLimit = theMap.localBounds.min + new Vector3(halfWidth, halfHeight);
+        topRightLimit = theMap.localBounds.max + new Vector3(-halfWidth, -halfHeight);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
     }
 }
