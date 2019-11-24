1d31ba30e91e59929c6d2bf4dc9a28fd6999ed87
Ricardo <ricardoglr664@gmail.com>
2019-11-23 18:06:39 -0600

    Camera Controller
    Now the camera follows the player in the first scene

---
+++ RPG1/Assets/Script Folder/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    void Start()
+    {
+        target = PlayerController.instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
