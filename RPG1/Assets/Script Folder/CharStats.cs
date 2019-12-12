58b01d010070705d47884ef300014b618b81b7b1
Ricardo <ricardoglr664@gmail.com>
2019-12-11 19:38:19 -0600

    Level Up System
    -Gaining stats on Level Up

--- RPG1/Assets/Script Folder/CharStats.cs
+++ RPG1/Assets/Script Folder/CharStats.cs
@@ -8,14 +8,17 @@ public class CharStats : MonoBehaviour
     public int playerLevel = 1;
     public int currentEXP;
     public int[] expToNextLevel;
-    public int maxLevel = 100;
+    public int maxLevel = 10;
 
     public int baseEXP = 1000;
 
     public int currentHP;
     public int maxHP = 100;
     public int currentMP;
+
     public int maxMP = 30;
+    public int[] mpLvlBonus;
+
     public int strength;
     public int defence;
     public int wpnPwr;
@@ -41,17 +44,39 @@ public class CharStats : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.K))
         {
-            AddExp(500);
+            AddExp(1000);
         }
     }
 
     public void AddExp(int expToAdd)
     {
         currentEXP += expToAdd;
-        if(currentEXP>expToNextLevel[playerLevel])
+        if (playerLevel < maxLevel)
         {
-            currentEXP -= expToNextLevel[playerLevel];
-            playerLevel++;
+            if (currentEXP > expToNextLevel[playerLevel])
+            {
+                currentEXP -= expToNextLevel[playerLevel];
+                playerLevel++;
+
+                //determine whether to add to str or def based on odd or event
+                if (playerLevel % 2 == 0)
+                {
+                    strength++;
+                }
+                else
+                {
+                    defence++;
+                }
+                maxHP = (int)(maxHP * 1.05f);
+                currentHP = maxHP;
+
+                maxMP += mpLvlBonus[playerLevel];
+                currentMP = maxMP;
+            }
+        }
+        if(playerLevel >= maxLevel)
+        {
+            currentEXP = 0;
         }
     }
 }
