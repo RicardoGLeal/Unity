21bdfe5d3c9858ea152e501791b6a9340b257086
Ricardo <ricardoglr664@gmail.com>
2019-12-12 20:29:31 -0600

    Levelling Up
    Creating a Game Manager

---
+++ RPG1/Assets/Script Folder/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager instance;
+    public CharStats[] playerStats; 
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
